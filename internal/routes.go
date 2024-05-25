db35d026053d8c1c6887cc3fde74d7f0f6e41ebe
Kevin Destrem <kefniark@gmail.com>
2024-05-25 08:14:01 +0000

    Serve static folder

--- internal/routes.go
+++ internal/routes.go
@@ -5,18 +5,18 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/kefniark/go-web-server/gen/api/apiconnect"
-	productsService "github.com/kefniark/go-web-server/internal/api/products"
-	usersService "github.com/kefniark/go-web-server/internal/api/users"
+	api "github.com/kefniark/go-web-server/internal/api"
+
 	"github.com/kefniark/go-web-server/internal/core"
 	"github.com/kefniark/go-web-server/internal/middlewares"
 )
 
-func registerRoutes(mux *http.ServeMux, options *core.ServerOptions) {
+func registerAPIRoutes(mux *http.ServeMux, options *core.ServerOptions) {
 	interceptors := connect.WithInterceptors(middlewares.WithDevLogInterceptor(options))
 
-	path, handler := apiconnect.NewUsersHandler(usersService.NewUserService(options), interceptors)
+	path, handler := apiconnect.NewUsersHandler(api.NewUserService(options), interceptors)
 	mux.Handle(path, handler)
 
-	path, handler = apiconnect.NewProductsHandler(productsService.NewProductService(options), interceptors)
+	path, handler = apiconnect.NewProductsHandler(api.NewProductService(options), interceptors)
 	mux.Handle(path, handler)
 }
--- internal/server.go
+++ internal/server.go
@@ -3,11 +3,16 @@ package internal
 import (
 	"context"
 	"database/sql"
+	"net/http"
 	"os"
+	"time"
 
 	dbClient "github.com/kefniark/go-web-server/gen/db"
 	"github.com/kefniark/go-web-server/internal/core"
+	"github.com/kefniark/go-web-server/internal/middlewares"
 	"github.com/rs/zerolog"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 
 	// embed database schema.
 	_ "embed"
@@ -16,13 +21,47 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const addr = "localhost:5550"
+const defaultTimeout = 5 * time.Second
+
+func NewServer() {
+	options := newServerOptions()
+	logger := options.Logger
+	logger.Debug().Msg("Initialize Server")
+
+	// middlewares (http) & interceptors (connect)
+	middlewareProvider := middlewares.NewMiddlewareProvider()
+	middlewareProvider.Add(middlewares.CORS(options))
+	middlewareProvider.Add(middlewares.Auth(options))
+
+	// create router
+	mux := http.NewServeMux()
+	registerAPIRoutes(mux, options)
+
+	// HTTP listen and serve
+	server := &http.Server{
+		Addr:              addr,
+		ReadHeaderTimeout: defaultTimeout,
+		Handler:           middlewareProvider.Apply(h2c.NewHandler(mux, &http2.Server{})),
+	}
+
+	// Static Files
+	mux.Handle("/", http.FileServer(http.Dir("./static")))
+	logger.Info().Msg("Serving static files on /")
+
+	logger.Info().Msgf("Listening on %s", addr)
+	if err := server.ListenAndServe(); err != nil {
+		logger.Panic().Err(err).Msgf("Cannot start server and listen on %s", addr)
+	}
+}
+
 //go:embed db/schema.sql
 var dbSchema string
 
 func newServerOptions() *core.ServerOptions {
 	logger := newLogger()
 
-	db, err := newDatabase()
+	db, err := newDatabase(logger)
 	if err != nil {
 		logger.Panic().Err(err).Msg("Cannot initialize Database connection")
 	}
@@ -33,14 +72,16 @@ func newServerOptions() *core.ServerOptions {
 	}
 }
 
-func newDatabase() (*dbClient.Queries, error) {
-	db, err := sql.Open("sqlite3", ":memory:")
+func newDatabase(logger *zerolog.Logger) (*dbClient.Queries, error) {
+	// db, err := sql.Open("sqlite3", ":memory:")
+	db, err := sql.Open("sqlite3", "dev.db")
 	if err != nil {
 		return nil, err
 	}
 
+	// apply db schema
 	if _, err = db.ExecContext(context.Background(), dbSchema); err != nil {
-		return nil, err
+		logger.Warn().Err(err).Msg("Cannot apply database schema")
 	}
 
 	return dbClient.New(db), nil
