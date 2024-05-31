c30e6fd83481dda692e1077b7d26bffe3d08312e
Kevin Destrem <kefniark@gmail.com>
2024-05-31 10:17:35 +0000

    replace daisyUI by flowbite

--- example/tools/nodejs/tailwind.config.js
+++ example/tools/nodejs/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../../**/*.{css,templ,html}"],
+  content: ["../../**/*.{css,templ,html}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
   daisyui: {
     themes: ["night"],
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/typography")],
 };
